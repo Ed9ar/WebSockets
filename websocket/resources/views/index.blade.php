a0e81ae7a98044a9efb1ea8281b7ad1816d51b8c
EmilianoR <emiroldanp@hotmail.com>
2021-04-25 18:01:59 -0500

    Diseño dashboard

--- websocket/resources/views/index.blade.php
+++ websocket/resources/views/index.blade.php
@@ -1,19 +1,42 @@
 <!doctype html>
 <meta charset='utf-8'>
-<link href='css/dragula.css' rel='stylesheet' type='text/css' />
-<link href='css/example.css' rel='stylesheet' type='text/css' />
-<title>dragula</title>
+{{-- <link href='../css/dragula.css' rel='stylesheet' type='text/css' />
+<link href='../css/example.css' rel='stylesheet' type='text/css' /> --}}
+<link rel="stylesheet" href="{{ asset('/css/dragula.css')}}">
+<link rel="stylesheet" href="{{ asset('/css/example.css')}}">
+<title>Dashboard</title>
 
-<div class='examples'>
-  <script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1673&serve=C6AILKT&placement=githubcombevacqua" id="_carbonads_js"></script>
-  <div class='parent'>
-    <label for='hy'>Move stuff between these two containers. Note how the stuff gets inserted near the mouse pointer? Great stuff.</label>
+  <div class='parent center'>
+    <label for='hy'>Dashboard de pedidos y entregas</label>
     <div class='wrapper'>
       <div id='left-defaults' class='container'>
+        <h2>1. Salida de planta</h2>
         <div>You can move these elements between these two containers</div>
         <div>Moving them anywhere else isn't quite possible</div>
       </div>
       <div id='right-defaults' class='container'>
+        <h2>2. En Local Delivery Center</h2>
+        <div>There's also the possibility of moving elements around in the same container, changing their position</div>
+        <div>This is the default use case. You only need to specify the containers you want to use</div>
+        <div>More interactive use cases lie ahead</div>
+        <div>Moving <code>&lt;input/&gt;</code> elements works just fine. You can still focus them, too. <input placeholder='See?' /></div>
+        <div>Make sure to check out the <a href='https://github.com/bevacqua/dragula#readme'>documentation on GitHub!</a></div>
+      </div>
+      <div id='left-defaults' class='container'>
+        <h2>3. En proceso de entrega</h2>
+        <div>You can move these elements between these two containers</div>
+        <div>Moving them anywhere else isn't quite possible</div>
+      </div>
+      <div id='right-defaults' class='container'>
+        <h2>4. Entregado</h2>
+        <div>There's also the possibility of moving elements around in the same container, changing their position</div>
+        <div>This is the default use case. You only need to specify the containers you want to use</div>
+        <div>More interactive use cases lie ahead</div>
+        <div>Moving <code>&lt;input/&gt;</code> elements works just fine. You can still focus them, too. <input placeholder='See?' /></div>
+        <div>Make sure to check out the <a href='https://github.com/bevacqua/dragula#readme'>documentation on GitHub!</a></div>
+      </div>
+      <div id='right-defaults' class='container'>
+        <h2>5. Fallido</h2>
         <div>There's also the possibility of moving elements around in the same container, changing their position</div>
         <div>This is the default use case. You only need to specify the containers you want to use</div>
         <div>More interactive use cases lie ahead</div>
@@ -21,15 +44,6 @@
         <div>Make sure to check out the <a href='https://github.com/bevacqua/dragula#readme'>documentation on GitHub!</a></div>
       </div>
     </div>
-    <pre>
-      <code>
-dragula([document.getElementById(left), document.getElementById(right)]);
-      </code>
-    </pre>
   </div>
-
-
-
-</div>
-<script src='../js/dragula.js'></script>
-<script src='../js/example.min.js'></script>
+<script src="{{ asset('/js/dragula.js')}}"></script>
+<script src="{{ asset('/js/example.js')}}"></script>
