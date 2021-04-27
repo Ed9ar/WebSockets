ffda0539e417ba56660eb2a484287d90901355e8
EmilianoR <emiroldanp@hotmail.com>
2021-04-26 19:34:34 -0500

    Drag & drop enabled

---
+++ websocket/public/js/dashboard.js
@@ -0,0 +1,7 @@
+dragula([document.getElementById('linea-planta'), document.getElementById('linea-ldc'),
+        document.getElementById('linea-entrega'),document.getElementById('linea-entregado'),
+        document.getElementById('linea-fallido')],
+        {
+            revertOnSpill: true
+        });
+
